99ee2f4dedc3c4190ee2b896d3f9c81aac802a29
augustoherrera99 <augustofherrera@hotmail.com>
2022-10-28 01:34:31 -0300

    Agregar archivos de proyecto.

---
+++ pryHerreraAugustoSp2_Ferro/frcBoleteria.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace pryHerreraAugustoSp2_Ferro
+{
+    partial class frcBoleteria
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frcBoleteria));
+            this.lblDistancia = new System.Windows.Forms.Label();
+            this.lblDias = new System.Windows.Forms.Label();
+            this.txtDistancia = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.numDias = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numDias)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDistancia
+            // 
+            this.lblDistancia.AutoSize = true;
+            this.lblDistancia.Location = new System.Drawing.Point(53, 37);
+            this.lblDistancia.Name = "lblDistancia";
+            this.lblDistancia.Size = new System.Drawing.Size(84, 13);
+            this.lblDistancia.TabIndex = 0;
+            this.lblDistancia.Text = "Distancia en Km";
+            // 
+            // lblDias
+            // 
+            this.lblDias.AutoSize = true;
+            this.lblDias.Location = new System.Drawing.Point(50, 75);
+            this.lblDias.Name = "lblDias";
+            this.lblDias.Size = new System.Drawing.Size(87, 13);
+            this.lblDias.TabIndex = 1;
+            this.lblDias.Text = "Dias de Estancia";
+            // 
+            // txtDistancia
+            // 
+            this.txtDistancia.Location = new System.Drawing.Point(143, 34);
+            this.txtDistancia.Name = "txtDistancia";
+            this.txtDistancia.Size = new System.Drawing.Size(105, 20);
+            this.txtDistancia.TabIndex = 2;
+            this.txtDistancia.TextChanged += new System.EventHandler(this.txtDistancia_TextChanged);
+            this.txtDistancia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDistancia_KeyPress);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Enabled = false;
+            this.btnCalcular.Location = new System.Drawing.Point(234, 112);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // numDias
+            // 
+            this.numDias.Location = new System.Drawing.Point(144, 75);
+            this.numDias.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numDias.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numDias.Name = "numDias";
+            this.numDias.Size = new System.Drawing.Size(34, 20);
+            this.numDias.TabIndex = 5;
+            this.numDias.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // frcBoleteria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 146);
+            this.Controls.Add(this.numDias);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtDistancia);
+            this.Controls.Add(this.lblDias);
+            this.Controls.Add(this.lblDistancia);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frcBoleteria";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Venta de boletos";
+            this.Load += new System.EventHandler(this.frcBoleteria_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numDias)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDistancia;
+        private System.Windows.Forms.Label lblDias;
+        private System.Windows.Forms.TextBox txtDistancia;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.NumericUpDown numDias;
+    }
+}
+
---
+++ pryHerreraAugustoSp2_Ferro/frcBoleteria.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace pryHerreraAugustoSp2_Ferro
+{
+    public partial class frcBoleteria : Form
+    {
+        public frcBoleteria()
+        {
+            InitializeComponent();
+        }
+
+        private void frcBoleteria_Load(object sender, EventArgs e)
+        {
+            txtDistancia.Text = "";
+            numDias.Value = 1;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            float Precio = 0;
+            float Distancia = float.Parse(txtDistancia.Text);
+            int Estancia = int.Parse(numDias.Text);
+
+            Precio = Distancia * 5;
+
+            if (Distancia >= 100 && Estancia >= 7)
+            {
+                Precio = Precio * 0.5f;
+                MessageBox.Show("El precio de su boleto es de $" + Precio.ToString() + "\r\n" + "\r\n" + "(Descuento del 50%)", "Monto a pagar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtDistancia.Text = "";
+                numDias.Value = 1;  
+            }
+            else
+            {
+                MessageBox.Show("El precio de su boleto es de $" + Precio.ToString(), "Monto a pagar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtDistancia.Text = "";
+                numDias.Value = 1;
+            }
+        }
+
+        private void txtDistancia_TextChanged(object sender, EventArgs e)
+        {
+            if (txtDistancia.Text != "")
+            {
+                btnCalcular.Enabled = true;
+            }
+            else
+            {
+                btnCalcular.Enabled = false;
+            }
+        }
+
+        private void txtDistancia_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar) && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+
+        }
+    }
+}
